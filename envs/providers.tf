d9c13e7fc56f7a8eeffb23293475cd7a465a6cb3
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-05-09 18:55:45 +0900

    :recycle: hashicorp/aws ~> 6.0

---
+++ envs/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "admin"
+  region  = "ap-northeast-1"
+  default_tags {
+    tags = {
+      Env                = "prd"
+      System             = "aws-transcribe-pii-reduction-by-glue"
+      ManagedByTerraform = "true"
+    }
+  }
+}
