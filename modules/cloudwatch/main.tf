1ba2b12c5fbc4a7c7256ac83bffc9649f29ee973
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-05-14 05:26:00 +0000

    :memo: lambdaログ追加

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -1,6 +1,7 @@
 /************************************************************
 CloudWatch Logs
 ************************************************************/
+### Step Functions StateMachine
 resource "aws_cloudwatch_log_group" "sf_logs" {
   name                        = "/aws/vendedlogs/states/transcribe-glue-databrew-state-machine-Logs"
   log_group_class             = "STANDARD"
@@ -9,5 +10,17 @@ resource "aws_cloudwatch_log_group" "sf_logs" {
   skip_destroy                = false
   tags = {
     Name = "/aws/vendedlogs/states/transcribe-glue-databrew-state-machine-Logs"
+  }
+}
+
+### Lambda
+resource "aws_cloudwatch_log_group" "lambda_converter_logs" {
+  name                        = "/aws/lambda/transcribe-json-to-csv"
+  log_group_class             = "STANDARD"
+  retention_in_days           = 7
+  deletion_protection_enabled = false
+  skip_destroy                = false
+  tags = {
+    Name = "/aws/lambda/transcribe-json-to-csv"
   }
 }
--- modules/cloudwatch/outputs.tf
+++ modules/cloudwatch/outputs.tf
@@ -1,3 +1,7 @@
 output "arn_sflogs" {
   value = aws_cloudwatch_log_group.sf_logs.arn
 }
+
+output "name_lambda_loggroup" {
+  value = aws_cloudwatch_log_group.lambda_converter_logs.tags_all.Name
+}
