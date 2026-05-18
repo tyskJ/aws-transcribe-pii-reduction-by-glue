329e296b0281697deaf54a663689aace2839a9dc
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-05-18 23:33:29 +0900

    :memo: ...

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,3 +1,3 @@
-# output "arn_json_converter_lambda" {
-#   value = aws_lambda_function.json_converter.arn
-# }
+output "arn_json_converter_lambda" {
+  value = aws_lambda_function.json_converter.arn
+}
