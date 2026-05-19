dcf14b85f61d507f1fa5dba6321227e1737f9192
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-05-19 09:28:01 +0000

    :memo: input追加

--- modules/eventbridge/variables.tf
+++ modules/eventbridge/variables.tf
@@ -10,10 +10,18 @@ variable "glue_src_bucket_name" {
   type = string
 }
 
+variable "glue_dst_bucket_name" {
+  type = string
+}
+
 variable "sf_arn" {
   type = string
 }
 
 variable "eventbridge_rule_role_arn" {
+  type = string
+}
+
+variable "glue_databrew_role_arn" {
   type = string
 }
