2e72fdf7c180d20b966d5b0f905a6281f7866b90
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-05-19 06:51:39 +0000

    :memo: 不要なバケット削除

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -9,18 +9,6 @@ output "name_transcribe_dst_bucket" {
   value = aws_s3_bucket.transcribe_dst.id
 }
 
-output "name_translate_md_bucket" {
-  value = aws_s3_bucket.translate_md.id
-}
-
-output "name_translate_en_bucket" {
-  value = aws_s3_bucket.translate_en.id
-}
-
-output "name_translate_jp_bucket" {
-  value = aws_s3_bucket.translate_jp.id
-}
-
 output "name_glue_src_bucket" {
   value = aws_s3_bucket.glue_src.id
 }
@@ -35,19 +23,6 @@ output "arn_transcribe_src_bucket" {
 output "arn_transcribe_dst_bucket" {
   value = aws_s3_bucket.transcribe_dst.arn
 }
-
-output "arn_translate_md_bucket" {
-  value = aws_s3_bucket.translate_md.arn
-}
-
-output "arn_translate_en_bucket" {
-  value = aws_s3_bucket.translate_en.arn
-}
-
-output "arn_translate_jp_bucket" {
-  value = aws_s3_bucket.translate_jp.arn
-}
-
 
 output "arn_glue_src_bucket" {
   value = aws_s3_bucket.glue_src.arn
